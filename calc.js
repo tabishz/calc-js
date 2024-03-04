7d77b66fbaef5b78191f2916e04068c8c11b9edb
Tabish Zaidi <tabishz@gmail.com>
2024-03-03 21:04:18 -0700

    trying dynamic font sizing for display

--- calc.js
+++ calc.js
@@ -2,12 +2,12 @@ const display = document.getElementById('display');
 
 const numberBtnHandler = (event) => {
   const number = parseInt(event.target.id);
-  display.value += number;
+  display.textContent += number;
   resizeDisplayFonts();
 }
 
 const clearDisplayBtnHandler = () => {
-  display.value = '';
+  display.textContent = '';
 }
 
 document.querySelectorAll('.num-btn').forEach(
@@ -17,8 +17,8 @@ document.querySelectorAll('.num-btn').forEach(
 );
 
 const resizeDisplayFonts = () => {
-  const textLength = display.value.length;
-  const fontSize = Math.min(5, 5 - textLength / 4);
+  const textLength = display.textContent.length;
+  const fontSize = Math.max(2, 5 - textLength / 4);
   display.style.fontSize = fontSize + 'em';
   console.log(`setting font size to ${fontSize}em`);
 };
